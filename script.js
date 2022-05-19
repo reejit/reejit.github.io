dfbb88ac86c09f093a49de3ace9c6754a8a8da05
reejit <70316178+reejit@users.noreply.github.com>
2022-05-19 08:43:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+var timeDisplay = document.getElementById("time");
+function refreshTime() {
+  var dateString = new Date().toLocaleString("en-US", {timeZone: "America/Sao_Paulo"});
+  var formattedString = dateString.replace(", ", " - ");
+  timeDisplay.innerHTML = formattedString;
+}
+
+setInterval(refreshTime, 1000);
+
+
 var rando = document.getElementById("place");
 var arr = ["Noamundi", "Kolkata","India", "Jharkhand", "West Bengal", "Ranaghat", "Ranchi", "Chaibasa", "Home", "School", "Tution", "Hotel", "Bed"];
 function random() {
