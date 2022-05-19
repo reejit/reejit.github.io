007866a8212a76f16c6a02646d9c21c7b1c0c324
reejit <70316178+reejit@users.noreply.github.com>
2022-05-19 09:07:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var timeDisplay = document.getElementById("time");
 function refreshTime() {
-  var dateString = new Date().toLocaleString("en-US", {timeZone: "America/Sao_Paulo"});
+  var dateString = new Date().toLocaleString("en-US", {timeZone: "Asia/Kolkata"});
   var formattedString = dateString.replace(", ", " - ");
   timeDisplay.innerHTML = formattedString;
 }
