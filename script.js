d8d0d7f34e3ca17711ddec5d7e1e8fe5f42963ff
reejit <70316178+reejit@users.noreply.github.com>
2022-05-19 09:13:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ setInterval(refreshTime, 1000);
 
 
 var rando = document.getElementById("place");
-var arr = ["Noamundi", "Kolkata","India", "Jharkhand", "West Bengal", "Ranaghat", "Ranchi", "Chaibasa", "Home", "School", "Tution", "Hotel", "Bed"];
+var arr = ["at <b>Noamundi</b>", "at <b>Kolkata</b>","in <b>India</b>", "in <b>Jharkhand</b>", "in <b>West Bengal</b>", "at <b>Ranaghat</b>", "at <b>Ranchi</b>", "at <b>Chaibasa</b>", "at <b>Home</b>", "at <b>School</b>", "at <b>Tution</b>", "in a <b>Hotel</b>", "on <b>bed</b>", "in <b>bed</b>];
 function random() {
     var rand = arr[Math.floor(Math.random() * arr.length)];
     rando.innerHTML = rand;
