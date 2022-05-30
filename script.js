4bb87384c018f170a22e671d95080a84d66fa9ef
reejit <70316178+reejit@users.noreply.github.com>
2022-05-30 13:28:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -95,26 +95,12 @@ document.getElementById("second").remove();
 }
 
 
-$(window).load(function(){
-            setTimeout(function(){
-                $('#preloader').velocity({
-                    opacity : 0.1,
-                    translateY: "-80px"
-                }, {
-                    duration: 400,
-                    complete: function(){
-                    $('#hola').velocity({
-                    translateY : "-100%"
-                }, {
-                    duration: 1000,
-                    easing: [0.7,0,0.3,1],
-                    complete: function(){
-                        $('.home').addClass('animate-border divide');
-                    }
-                })  
-                    }
-                })
-            },1000)
-        })
+var loader = document.querySelector(".loader")
+
+window.addEventListener("load", vanish);
+
+function vanish() {
+  loader.classList.add("disppear");
+}
 
 
