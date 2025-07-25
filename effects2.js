29c16de159f4351f803385a6c75069db525f6957
Reejit <guardiansofspartax@gmail.com>
2025-07-25 11:50:56 +0530

    first better alternative

---
+++ effects2.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const imgSrc = "https://lanyard.cnrad.dev/api/1296550727652610189?bg=000000&animated=true&hideActivity=true&showDisplayName=true";
+    const imgElement = document.getElementById('rpc');
+    imgElement.src = imgSrc;
+});
